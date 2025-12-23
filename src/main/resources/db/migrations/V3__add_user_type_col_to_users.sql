a56768815222a380adc576e01050debf1606d498
Emelyanov Michael <micxem@yandex.ru>
2025-12-23 16:09:31 +0300

    сделал скрипт идемпотентным

--- src/main/resources/db/migrations/V3__add_user_type_col_to_users.sql
+++ src/main/resources/db/migrations/V3__add_user_type_col_to_users.sql
@@ -1,6 +1,9 @@
+-- Добавляем столбец, если его ещё нет
 ALTER TABLE IF EXISTS rightsflow.users
-    ADD COLUMN user_type character varying(10) NOT NULL DEFAULT 'USER';
+    ADD COLUMN IF NOT EXISTS user_type character varying(10) NOT NULL DEFAULT 'USER';
 
+-- Добавляем CHECK-ограничение, если его ещё нет
 ALTER TABLE IF EXISTS rightsflow.users
-    ADD CONSTRAINT chk_user_type CHECK (user_type in ('USER','SERVICE'))
+    ADD CONSTRAINT IF NOT EXISTS chk_user_type
+    CHECK (user_type IN ('USER', 'SERVICE'))
     NOT VALID;
