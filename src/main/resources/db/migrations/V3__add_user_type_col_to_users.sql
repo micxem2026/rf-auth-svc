30ac343df80c3c35430df95627306e8ca70eada5
Emelyanov Michael <micxem@yandex.ru>
2025-12-23 16:21:12 +0300

    сделал скрипт идемпотентным

--- src/main/resources/db/migrations/V3__add_user_type_col_to_users.sql
+++ src/main/resources/db/migrations/V3__add_user_type_col_to_users.sql
@@ -3,7 +3,17 @@ ALTER TABLE IF EXISTS rightsflow.users
     ADD COLUMN IF NOT EXISTS user_type character varying(10) NOT NULL DEFAULT 'USER';
 
 -- Добавляем CHECK-ограничение, если его ещё нет
-ALTER TABLE IF EXISTS rightsflow.users
-    ADD CONSTRAINT IF NOT EXISTS chk_user_type
-    CHECK (user_type IN ('USER', 'SERVICE'))
-    NOT VALID;
+DO $$
+BEGIN
+    -- Проверяем, существует ли уже ограничение с таким именем
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'chk_user_type'
+    ) THEN
+        -- Если не существует, создаем его
+        ALTER TABLE rightsflow.users
+            ADD CONSTRAINT chk_user_type
+                CHECK (user_type IN ('USER', 'SERVICE'));
+    END IF;
+END $$;
